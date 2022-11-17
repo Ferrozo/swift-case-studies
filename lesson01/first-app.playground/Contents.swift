d13258710034c00639728b3d8c6ef6f6d6070bd1
Ferrozo <ferrozo.c@gmail.com>
2022-11-17 01:37:04 +0100

    make first lesson about variable

---
+++ lesson01/first-app.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+var firstName = "Wilson"
+var lastName = "Ferrozo"
+var age = 20
+
+print("Welcome " + firstName + " " +  lastName + " to swift lang")
+
+if(age < 18) {
+    print("Under age")
+}else {
+    print("Adult age", age)
+}
